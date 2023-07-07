c4aa8f8a336c686579e8b217fecbee104468fe10
Ricky9702 <khuong93qt@gmail.com>
2023-07-08 00:29:30 +0700

    initial comit

---
+++ SocialMediaApp/app/src/main/java/com/example/h2ak/Fragments/CreateFragment.java
@@ -0,0 +1,33 @@
+package com.example.h2ak.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.h2ak.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateFragment extends Fragment {
+
+    public CreateFragment() {
+        // Required empty public constructor
+    }
+
+    public static CreateFragment newInstance() {
+        return new CreateFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create, container, false);
+    }
+}
